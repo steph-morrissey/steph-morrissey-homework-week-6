55a67f2e14095b41e23dcd3b5ad0fb67edf6b032
Stephanie Morrissey <sgmorrissey098@gmail.com>
2020-05-07 21:40:11 +0100

    Uses ajax to retrieve weather data for current day and displays that within window

--- assets/script.js
+++ assets/script.js
@@ -1,69 +1,90 @@
 // API mock data for ajax call
 const mockCityData = [
   {
-    coord: { lon: 139, lat: 35 },
+    coord: { lon: -122.08, lat: 37.39 },
     weather: [
       {
         id: 800,
         main: "Clear",
         description: "clear sky",
-        icon: "01n",
+        icon: "01d",
       },
     ],
     base: "stations",
     main: {
-      temp: 281.52,
-      feels_like: 278.99,
-      temp_min: 280.15,
-      temp_max: 283.71,
-      pressure: 1016,
-      humidity: 93,
+      temp: 282.55,
+      feels_like: 281.86,
+      temp_min: 280.37,
+      temp_max: 284.26,
+      pressure: 1023,
+      humidity: 100,
     },
+    visibility: 16093,
     wind: {
-      speed: 0.47,
-      deg: 107.538,
+      speed: 1.5,
+      deg: 350,
     },
     clouds: {
-      all: 2,
+      all: 1,
     },
-    dt: 1560350192,
+    dt: 1560350645,
     sys: {
-      type: 3,
-      id: 2019346,
-      message: 0.0065,
-      country: "JP",
-      sunrise: 1560281377,
-      sunset: 1560333478,
+      type: 1,
+      id: 5122,
+      message: 0.0139,
+      country: "US",
+      sunrise: 1560343627,
+      sunset: 1560396563,
     },
-    timezone: 32400,
-    id: 1851632,
-    name: "Shuzenji",
+    timezone: -25200,
+    id: 420006353,
+    name: "Mountain View",
     cod: 200,
   },
 ];
-// Declare ajax querySearch
-let cityName;
-
+// API mock data for uv index value
+const uvIndexData = [
+  {
+    lat: 38.75,
+    lon: 40.25,
+    date_iso: "2017-06-23T12:00:00Z",
+    date: 1498219200,
+    value: 10.16,
+  },
+];
+// Variable for current day weather date
+const currentDate = moment().format("(DD/MM/YYYY)");
+// Variable for ajax uvIndex query property
+const uvQueryURL =
+  "http://api.openweathermap.org/data/2.5/uvi?appid={appid}&lat={lat}&lon={lon}";
+// Empty array for storing recent searches
 let recentCitySearches = [];
 // Function that takes in response from form submission
 function citySearch(city) {
   city.preventDefault();
+  // Target users city input and transform to lower case
   const citySearch = city.target[0].value;
   const cityName = citySearch.toLowerCase();
+  // API key
   const apiKey = "43c5e9000139b9bdf38b1549672e1492";
+  // Query URL which will be used to make ajax call
   const queryURL =
     "https://api.openweathermap.org/data/2.5/weather?q=" +
     cityName +
     "&appid=" +
     apiKey;
-
+  // Ajax call
   $.ajax({
     url: queryURL,
     method: "GET",
-  }).then(example);
-
-  function example(response) {
-    console.log(response);
+  }).then(renderCurrentWeatherData);
+  // Function that is executed once data has been received
+  function renderCurrentWeatherData(response) {
+    $("#cityName").text(response.name + " " + currentDate);
+    $("#temperature").text(response.main.temp);
+    $("#humidity").text(response.main.humidity);
+    $("#windTemp").text(response.wind.deg);
+    $("#uvIndex").text(uvIndexData[0].value);
   }
 }
 // Add recent searches to localStorage
